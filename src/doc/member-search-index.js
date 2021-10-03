dcc80912eb72486f9d73353ddcfebb5664cf0eef
ice-kylin <wminid@yeah.net>
2021-10-03 18:49:21 +0100

    first commit

---
+++ src/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"l":"main(String[])","h":"","u":"main(java.lang.String[])"},{"l":"注释()","h":"","u":"%3Cinit%3E()"}]
---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"所有类","h":"","u":"allclasses-index.html"},{"l":"注释","h":"","u":""}]
