0d220fb80ad1606cacb28bebec3ca60f101ea918
ice-kylin <wminid@yeah.net>
2021-10-05 15:05:40 +0100

    update

---
+++ src/basicgrammar/Variable.java
@@ -0,0 +1,25 @@
+package basicgrammar;
+
+/*
+# 变量
+
+- 变量是内存中的一个储存区域
+- 该区域的数据可以在同一类型范围内不断变化
+- 变量是程序中最基本的存储单元，包含
+  - 变量类型
+  - 变量名
+  - 存储的值
+
+# 变量的使用
+
+- Java 定义变量的格式
+  - `数据类型 变量名 = 变量值;`
+ */
+public class Variable {
+    public static void main(String[] args) {
+        // 变量的定义
+        int myAge = 19;
+        // 变量的使用
+        System.out.println(myAge);
+    }
+}
