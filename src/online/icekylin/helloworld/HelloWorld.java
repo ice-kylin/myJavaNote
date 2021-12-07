55979b691d26e4777bae79d97595bf2f29b93af7
ice-kylin <wminid@yeah.net>
2021-12-07 05:52:54 +0000

    update

---
+++ src/online/icekylin/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package online.icekylin.helloworld;
+
+/**
+ * 
+ * @Description
+ * @author ice-kylin
+ * @date 2021年12月7日上午5:47:09
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Eclipse!");
+	}
+}
