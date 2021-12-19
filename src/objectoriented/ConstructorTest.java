7661e026ab3c7b9b6c2efa2f06b47f58a1c2e66e
icekylin <wminid@yeah.net>
2021-12-19 11:46:16 +0000

    update

--- src/objectoriented/ConstructorTest.java
+++ src/objectoriented/ConstructorTest.java
@@ -15,6 +15,13 @@ package objectoriented;
 - 一个类中定义的多个构造器，彼此构成重载
 - 一旦显式的定义了类的构造器之后，系统就不再提供默认的空参构造器了
 - 一个类中至少会有一个构造器
+
+## 属性赋值的先后顺序
+
+1. 默认初始化
+2. 显式初始化
+3. 构造器中赋值
+4. 通过 `对象.方法` 或 `对象.属性` 的方式赋值
  */
 public class ConstructorTest {
     public static void main(String[] args) {
@@ -28,7 +35,6 @@ class Person1 {
     int age;
 
     public Person1() {
-
     }
 
     public Person1(String n, int a) {
