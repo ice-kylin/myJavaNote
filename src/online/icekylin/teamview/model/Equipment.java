2d340351b6b98a979069a1933a17f289093f0447
icekylin <wminid@yeah.net>
2022-01-05 18:37:00 +0000

    update

---
+++ src/online/icekylin/teamview/model/Equipment.java
@@ -0,0 +1,5 @@
+package online.icekylin.teamview.model;
+
+public interface Equipment {
+    String getDescription();
+}
--- src/online/icekylin/teamview/util/TSUtility.java
+++ src/online/icekylin/teamview/util/TSUtility.java
@@ -9,7 +9,7 @@ public class TSUtility {
         boolean correctness = false;
         String userChoice;
 
-        System.out.println(msg);
+        System.out.print(msg);
 
         do {
             userChoice = scanner.nextLine();
@@ -21,9 +21,41 @@ public class TSUtility {
                 }
             }
 
-            System.out.println(errorMsg);
+            System.out.print(errorMsg);
         } while (!correctness);
 
         return userChoice;
+    }
+
+    private static int readInt(int min, int max, String msg, String errorMsg) {
+        int userChoice;
+
+        System.out.print(msg);
+
+        do {
+            try {
+                userChoice = scanner.nextInt();
+            } catch (NumberFormatException e) {
+                System.out.print(errorMsg);
+                continue;
+            }
+
+            if (userChoice >= min && userChoice <= max) {
+                break;
+            } else {
+                System.out.print(errorMsg);
+            }
+        } while (true);
+
+        return userChoice;
+    }
+
+    public static String readMenuSelection() {
+        return readString(new String[]{"1", "2", "3", "4"}, "请输入选项 (1-4)：", "选择错误，请重新输入 (1-4)：");
+    }
+
+    public static void readReturn() {
+        System.out.println("按下回车继续...");
+        scanner.nextLine();
     }
 }
